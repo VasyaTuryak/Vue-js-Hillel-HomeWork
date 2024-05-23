3b2b56923fe3de0f704d923c682036d3cb0e3716
VasyaTuryak <vasyaturyak87@gmail.com>
2024-05-24 01:16:01 +0300

    HW 2. NPM

---
+++ index.js
@@ -0,0 +1,18 @@
+//add next functions: addition,subtraction and multiplication.
+
+function additionTwoNumbers(NumberOne, numberTwo) {
+    return NumberOne+numberTwo;
+}
+
+function subtractionTwoNumbers(NumberOne, numberTwo) {
+    return NumberOne-numberTwo;
+}
+
+
+function multiplicationTwoNumbers(NumberOne, numberTwo) {
+    return NumberOne*numberTwo;
+}
+
+console.log(additionTwoNumbers(2, 2));
+console.log(subtractionTwoNumbers(2, 2));
+console.log(multiplicationTwoNumbers(2, 2));
