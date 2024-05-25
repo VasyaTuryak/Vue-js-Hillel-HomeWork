ca2839dc2737eee8dadd955811e5884fb57b882c
VasyaTuryak <vasyaturyak87@gmail.com>
2024-05-25 19:28:29 +0300

    Add component V2

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Card from "./components/Card.vue";
+</script>
+
+<template>
+
+  <Card number='1' title="title1 from App.vue" text="text1 from App.vue"  />
+  <Card number='2' title="title2 from App.vue"  />
+  <Card number='3'  text="text3 from App.vue"  />
+
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps(
+    ['number','title','text'
+    ]
+
+)
+</script>
+
+<template>
+  <div className="card" style="border: solid; margin-bottom: 5px">
+    <div className="card-body">
+      <p>PROPS number {{number}} </p>
+      <h4 className="card-title">hi</h4>
+      <p className="card-text">how are you?</p>
+      <br>
+      <p>Below I'm rendering props from App.vue</p>
+      <p v-if="title">Props title: {{title}}</p>
+      <p v-if="text">Props text: {{text}}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
