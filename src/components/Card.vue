51cdbd257f50eddbce1078ffceaaa9bddc85fc46
VasyaTuryak <vasyaturyak87@gmail.com>
2024-05-25 18:38:37 +0300

    Add component V1

---
+++ src/components/Card.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <div className="card" style="border: solid;">
+    <div className="card-body">
+      <h4 className="card-title">Card title</h4>
+      <p className="card-text">Some quick example text to build on the card</p>
+      <button type="button" className="btn btn-primary">Go somewhere</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
