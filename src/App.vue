6691164ca49acc104b8d6a5ff76c5da760d5d7c4
VasyaTuryak <vasyaturyak87@gmail.com>
2024-06-01 19:22:41 +0300

    rewrite CompositionAPI to OptionAPI

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,15 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import Card from "./components/Card.vue";
+<script>
+import Card from './components/Card.vue'
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  components: {Card}
+})
 </script>
 
 <template>
-
   <p>Rendering component CARD </p>
   <Card/>
-
-<!--  <div>-->
-<!--    <a href="https://vitejs.dev" target="_blank">-->
-<!--      <img src="/vite.svg" class="logo" alt="Vite logo" />-->
-<!--    </a>-->
-<!--    <a href="https://vuejs.org/" target="_blank">-->
-<!--      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />-->
-<!--    </a>-->
-<!--  </div>-->
-<!--  <HelloWorld msg="Vite + Vue" />-->
 </template>
 
 <style scoped>
