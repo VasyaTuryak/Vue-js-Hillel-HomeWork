befee379a94073c643f11632a25b13b23c575244
VasyaTuryak <vasyaturyak87@gmail.com>
2024-06-04 22:52:18 +0300

    homework, Group list

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+import ListGroup from "./components/ListGroup.vue";
+import {defineComponent} from "vue";
+export default defineComponent( {
+  components:{
+    ListGroup
+  },
+  data(){
+    return{
+      listOfChild: ['Alex','Peter','John','Oscar','Mark', 'Bred']
+    }
+  }
+})
+</script>
+
+<template>
+  <div style="border: solid;">
+    <h1>Parent component </h1>
+
+    <ListGroup style="width: 80%; margin-left: auto; margin-right: auto">
+      <li class="list-group-item"
+          :class="[(index%2===0)?'list-group-item-info':'list-group-item-warning']"
+          v-for="(child,index) in listOfChild" :key="index">
+        {{index+1}} - {{child}}
+      </li>
+    </ListGroup>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ListGroup.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "ListGroup",
+}
+</script>
+
+<template>
+<div style="border: solid">
+  <h2><b>Children component</b>, show list children through SLOT from parent component</h2>
+  <ul class="list-group">
+    <slot/>
+  </ul>
+</div>
+</template>
+
+<style scoped>
+
+</style>
