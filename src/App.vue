e555aba2e477cd05955dc03f4dd67d34e62e866c
VasyaTuryak <vasyaturyak87@gmail.com>
2024-06-13 02:53:19 +0300

    HW 15 Collapse

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Приклад компонента Collapse</h1>
+ <Collapse text="Це прихований текст" :opened="true"/>
+  </div>
+</template>
+
+<script>
+
+import {defineComponent} from "vue";
+import Collapse from "./components/Collapse.vue";
+
+export default defineComponent({
+  components: {Collapse}
+})
+</script>
+
+
+
+
---
+++ src/components/Collapse.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+  <div>
+    <p>
+      <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" >Link with href</a>
+    </p>
+    <div class="collapse" id="collapseExample" :class="opened && 'show'">
+      <div class="card card-body">
+        collapse me: {{text}}
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Collapse",
+  props:{
+    text:{
+      type: String,
+      required: false,
+      default: 'Here should be yor text'
+    },
+    opened:{
+      type: Boolean,
+      required: false,
+      default: true
+    },
+  }
+}
+</script>
+
